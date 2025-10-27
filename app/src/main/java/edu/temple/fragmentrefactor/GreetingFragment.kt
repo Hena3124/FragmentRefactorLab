9ca037a507899e1a3eb07e9675c191c6c1e54e07
Hena Patel <hena.patel0002@temple.edu>
2025-10-27 16:08:30 -0400

    First Update

---
+++ app/src/main/java/edu/temple/fragmentrefactor/GreetingFragment.kt
@@ -0,0 +1,41 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.util.zip.Inflater
+
+class GreetingFragment : Fragment(){
+
+    override fun onCreateView (
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return  inflater.inflate(R.layout.fragment_greeting,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        super.onCreate(savedInstanceState)
+
+        val displayTextView = view.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = view.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+    }
+
+}
